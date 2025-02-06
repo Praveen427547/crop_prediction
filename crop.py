8fe75030ecf76d93cf27e3f7b2d95f46a8aac861
Praveen <pathivadapraveen567@gmail.com>
2025-02-06 23:14:23 +0530

    Update crop.py

--- crop.py
+++ crop.py
@@ -97,6 +97,7 @@ crop_images = {
     "muskmelon" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMXXG-GCCjjh3ERJVAYkBAe0H-wAb3pXwXqQ&s.jpg",
     "maize" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpcPJn8xeSS5N4LBzA9JZv0TFkXJSDPxVIHQ&s.jpg",
     "orange" : "https://cdn.freshfruitportal.com/2021/07/WH_2008_PC08_0754_rgb-1024x683.jpg",
+    "cotton" : "https://media.istockphoto.com/id/589121090/photo/branch-of-ripe-cotton.jpg",
     # Add other crops and their URLs here
 }
 
